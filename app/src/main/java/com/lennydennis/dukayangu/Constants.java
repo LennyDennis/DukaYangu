c4d7f22c2d631f3b872bc1712b9971f372e0cd50
LennyDennis <lennydennis@gmail.com>
2020-03-10 23:16:38 +0300

    Add BestBuy API key in the gradle properties and hide gradle properties by adding it to the .gitignore file

---
+++ app/src/main/java/com/lennydennis/dukayangu/Constants.java
@@ -0,0 +1,6 @@
+package com.lennydennis.dukayangu;
+
+public class Constants {
+    public static final String BESTBUY_BASE_URL = "https://openapi.etsy.com/v2/";
+    public static final String BESTBUY_API_KEY = BuildConfig.BESTBUY_API_KEY;
+}
