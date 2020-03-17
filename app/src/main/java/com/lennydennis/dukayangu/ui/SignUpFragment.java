3721e391aeb80aeb92d456e79fca15b4e9de77dd
LennyDennis <lennydennis@gmail.com>
2020-03-17 21:33:49 +0300

    - Included firebase authenitication dependency in the app's build.gradle file - Enable user registration with email and password in the sign up fragment

--- app/src/main/java/com/lennydennis/dukayangu/ui/SignUpFragment.java
+++ app/src/main/java/com/lennydennis/dukayangu/ui/SignUpFragment.java
@@ -2,9 +2,11 @@ package com.lennydennis.dukayangu.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +15,12 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.lennydennis.dukayangu.R;
 
 import butterknife.BindView;
@@ -22,7 +29,7 @@ import butterknife.ButterKnife;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SignUpFragment extends Fragment {
+public class SignUpFragment extends Fragment implements View.OnClickListener {
 
     @BindView(R.id.sign_up_name) EditText signUpName;
     @BindView(R.id.sign_up_email) EditText signUpEmail;
@@ -31,6 +38,8 @@ public class SignUpFragment extends Fragment {
     @BindView(R.id.sign_up_gender) RadioGroup signUpGender;
     @BindView(R.id.sign_up_button) Button signUpButton;
     @BindView(R.id.already_registered_text) TextView signInText;
+
+    private FirebaseAuth mAuth;
 
     public SignUpFragment() {
         // Required empty public constructor
@@ -44,18 +53,57 @@ public class SignUpFragment extends Fragment {
         View view =  inflater.inflate(R.layout.fragment_sign_up, container, false);
         ButterKnife.bind(this, view);
 
-        signInText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        mAuth = FirebaseAuth.getInstance();
+
+        signInText.setOnClickListener(this);
+        signUpButton.setOnClickListener(this);
+
+        // Inflate the layout for this fragment
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.already_registered_text:
                 FragmentTransaction transaction = getFragmentManager().beginTransaction();
                 SignInFragment signInFragment = new SignInFragment();
 
                 transaction.replace(R.id.fragment_container, signInFragment);
                 transaction.addToBackStack(null);
                 transaction.commit();
-            }
-        });
-        // Inflate the layout for this fragment
-        return view;
+                break;
+            case R.id.sign_up_button:
+                createNewUser();
+        }
+    }
+
+    public void createNewUser(){
+        String name = signUpName.getText().toString().trim();
+        String email = signUpEmail.getText().toString().trim();
+        String password = signUpPassword.getText().toString().trim();
+        String cPassword  = confirmPassword.getText().toString().trim();
+        String gender = signUpGender.toString().trim();
+
+        if(TextUtils.isEmpty(name)){
+            Toast.makeText(getContext(),"Enter your name",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(getContext(),"Email field is empty",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(getContext(),"Registration successful",Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(getContext(),"Registration unsuccessful",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
