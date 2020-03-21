d3a94fe1f7b0664144f0ef2720116748b05cd160
LennyDennis <lennydennis@gmail.com>
2020-03-21 12:10:54 +0300

    - Enable reading from the firebase realtime database - Display the user name, email and phone in the account fragment

--- app/src/main/java/com/lennydennis/dukayangu/ui/AccountFragment.java
+++ app/src/main/java/com/lennydennis/dukayangu/ui/AccountFragment.java
@@ -1,12 +1,14 @@
 package com.lennydennis.dukayangu.ui;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,12 +16,37 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.lennydennis.dukayangu.R;
+import com.lennydennis.dukayangu.User;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AccountFragment extends Fragment {
+
+    private static final String TAG = "AccountFragment";
+
+    @BindView(R.id.userName)
+    TextView userName;
+    @BindView(R.id.userEmail)
+    TextView userEmail;
+    @BindView(R.id.userPhone)
+    TextView userPhone;
+
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private String userId;
 
     public AccountFragment() {
         // Required empty public constructor
@@ -34,8 +61,53 @@ public class AccountFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_account, container, false);
+        ButterKnife.bind(this, view);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        userId = firebaseUser.getUid();
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                if (firebaseUser != null) {
+                    Log.d(TAG, "Signed in");
+                } else {
+                    Log.d(TAG, "Signed out");
+                }
+            }
+        };
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                showUserAccount(dataSnapshot);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        return view;
+    }
+
+    private void showUserAccount(DataSnapshot dataSnapshot) {
+        for (DataSnapshot eachSnapShot : dataSnapshot.getChildren()) {
+            User user = new User();
+            user.setUserName(eachSnapShot.child(userId).getValue(User.class).getUserName());
+            user.setUserEmail(eachSnapShot.child(userId).getValue(User.class).getUserEmail());
+            user.setPhoneNumber(eachSnapShot.child(userId).getValue(User.class).getPhoneNumber());
+
+            userName.setText(user.getUserName());
+            userEmail.setText(user.getUserEmail());
+            userPhone.setText(user.getPhoneNumber());
+        }
     }
 
     @Override
